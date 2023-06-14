f9596fab98df9bd598f1e7e811bc09b3b8eb8a14
Breno Oliveira <brenolincon8@gmail.com>
2023-06-14 12:09:33 -0300

    Aula 04

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,10 @@
-import React from 'react'
+import { createRoot } from 'react-dom/client'
 import { App } from './App'
+
+const domNode = document.getElementById('root')
+const root = createRoot(domNode)
+
+root.render(<App />)
+
+// CÓDIGO ALTERADO PARA REMOVER ERRO NO CONSOLE DO NAVEGADOR
+// render(<App />, document.getElementById('root'))
